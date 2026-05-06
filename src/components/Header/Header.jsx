c44d25fa9ed037c6c99fe969c09099d7328d372f
GIT-CHEA002 <chhunsokchea317@gmail.com>
2026-05-07 02:39:34 +0700

    fixed ios scrolling issue

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ function Header() {
   return (
     <>
       <title>Home Page</title>
-      <nav className="fixed top-0 left-0 w-full z-50  bg-white  ">
+      <nav className="sticky top-0 left-0 w-full z-[999] pt-[env(safe-area-inset-top)] bg-white  ">
         <DesktopMenu
           navLinkClass={navLinkClass}
           setMobileMenu={setMobileMenu}
